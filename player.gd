52acce1a573dd0b3c2e2224a8fd440c39210df4f
yliris <mik041126@gmail.com>
2025-08-05 19:17:59 +0800

    stand/idle & walk state
    Animated stand/idle and walk state added.

--- player.gd
+++ player.gd
@@ -4,8 +4,19 @@ const SPEED = 70.0
 
 var input_vector = Vector2.ZERO
 
+@onready var animation_tree = $AnimationTree
+
 func _physics_process(delta):
 	input_vector = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
 	
+	if input_vector != Vector2.ZERO:
+		var direction_vector = Vector2(input_vector.x, -input_vector.y)
+		update_blend_positions(direction_vector)
+	
 	velocity = input_vector * SPEED
 	move_and_slide()
+
+func update_blend_positions(direction_vector):
+	animation_tree.set("parameters/StateMachine/MoveState/Stand/blend_position", direction_vector)
+	animation_tree.set("parameters/StateMachine/MoveState/Walk/blend_position", direction_vector)
+	animation_tree.set("parameters/StateMachine/MoveState/Sprint/blend_position", direction_vector)
