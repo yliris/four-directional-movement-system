3646a09f14d10c684e631648f6446463de212dab
yliris <mik041126@gmail.com>
2025-08-05 20:19:27 +0800

    movement update

--- player.gd
+++ player.gd
@@ -1,22 +1,26 @@
 extends CharacterBody2D
 
-const SPEED = 70.0
+const WALK_SPEED = 70.0
+const SPRINT_SPEED = 100.0
 
-var input_vector = Vector2.ZERO
+var input_walk_vector = Vector2.ZERO
+var input_sprint = false
 
 @onready var animation_tree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
 
 func _physics_process(delta):
-	input_vector = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
+	var state = playback.get_current_node()
+	input_walk_vector = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
+	input_sprint = Input.is_action_pressed("sprint")
 	
-	if input_vector != Vector2.ZERO:
-		var direction_vector = Vector2(input_vector.x, -input_vector.y)
+	if input_walk_vector != Vector2.ZERO:
+		var direction_vector = Vector2(input_walk_vector.x, -input_walk_vector.y)
 		update_blend_positions(direction_vector)
 	
-	velocity = input_vector * SPEED
+	velocity = input_walk_vector * WALK_SPEED
 	move_and_slide()
 
 func update_blend_positions(direction_vector):
 	animation_tree.set("parameters/StateMachine/MoveState/Stand/blend_position", direction_vector)
 	animation_tree.set("parameters/StateMachine/MoveState/Walk/blend_position", direction_vector)
-	animation_tree.set("parameters/StateMachine/MoveState/Sprint/blend_position", direction_vector)
